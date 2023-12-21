1139dff0b00f44c87d7db29b8ec24aa0ac8db352
DillTheBeast <dillonmaltese@gmail.com>
2023-12-21 01:22:36 -0500

    Library update + adding code to make all of stuff work together

--- FullControl/FullControl.ino
+++ FullControl/FullControl.ino
@@ -2,24 +2,20 @@
 #include "VoiceRecognitionV3.h"
 #include <Servo.h>
 
-/**        
-  Connection
-  Arduino    VoiceRecognitionModule
-   2   ------->     TX
-   3   ------->     RX
-*/
-
 VR myVR(2, 3);
 
 uint8_t records[7];
 uint8_t buf[64];
 
-int servoPin = 13; // Change the name of the pin variable
-Servo servo;       // Use Servo type for the servo motor
+int servoPin = 13;
+int servoPin1 = 9;
+Servo servo;
+Servo servo1;
 
-#define onRecord (0)
-#define offRecord (1)
+#define pcRecord (0)
+#define topRecord (1)
 #define callRecord (2)
+#define lightRecord (3)
 
 #define IDLE_STATE 0
 #define WAIT_FOR_JARVIS_STATE 1
@@ -99,8 +95,10 @@ void setup()
   Serial.begin(115200);
   Serial.println("Elechouse Voice Recognition V3 Module\r\nControl LED sample");
   
-  pinMode(servoPin, OUTPUT); // Use the servoPin for pin initialization
-  servo.attach(servoPin);    // Attach the servo motor to the correct pin
+  pinMode(servoPin, OUTPUT);
+  pinMode(servoPin1, OUTPUT);
+  servo.attach(servoPin);
+  servo1.attach(servoPin1);
     
   if(myVR.clear() == 0){
     Serial.println("Recognizer cleared.");
@@ -108,18 +106,6 @@ void setup()
     Serial.println("Not find VoiceRecognitionModule.");
     Serial.println("Please check connection and restart Arduino.");
     while(1);
-  }
-  
-  if(myVR.load((uint8_t)onRecord) >= 0){
-    Serial.println("onRecord loaded");
-  }
-  
-  if(myVR.load((uint8_t)offRecord) >= 0){
-    Serial.println("offRecord loaded");
-  }
-
-  if(myVR.load((uint8_t)callRecord) >= 0){
-    Serial.println("caseRecord loaded");
   }
 }
 
@@ -153,14 +139,26 @@ void loop()
     ret = myVR.recognize(buf, 50);
     if (ret > 0)
     {
-      if (buf[1] == onRecord && !jarvisSaid)
-      {
+      if (buf[1] == pcRecord && !jarvisSaid) {
         //Move servo to turn on
         servo.write(120);
         delay(1000);
         servo.write(0);
         currentState = IDLE_STATE;
         jarvisSaid = true;
+      }
+
+      if (buf[1] == topRecord) {
+        //Move servo to turn on
+        servo1.write(120);
+        delay(1000);
+        servo1.write(0);
+        currentState = IDLE_STATE;
+        jarvisSaid = true;
+      }
+
+      if (buf[1] == lightRecord) {
+        //Send power to leds
       }
       printVR(buf);
     }
