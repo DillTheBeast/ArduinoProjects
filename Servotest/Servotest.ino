f4b3cad53b7ef436cbb563789e7161ef2fad918c
DillTheBeast <dillonmaltese@gmail.com>
2024-01-06 21:07:31 -0500

    Update Servotest.ino

--- Servotest/Servotest.ino
+++ Servotest/Servotest.ino
@@ -1,15 +1,16 @@
 #include <Servo.h>
 
 Servo servo;
-#define SERVO_PIN 11  //servo connect to D9
+#define SERVO_PIN 9  //servo connect to D9
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   servo.attach(SERVO_PIN);
-  servo.write(170);
-  delay(100000);
+  servo.write(30);
+  delay(2000);
+  servo.write(0);
 }
 
 void loop() {
